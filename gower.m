61afdfa62827f4de6287af14d5cf29fd79652fb8
Yuchang Yang <deegir@126.com>
2019-05-11 22:09:15 +0800

    Add files via upload

---
+++ gower.m
@@ -0,0 +1,9 @@
+function g = gower(X, Y)
+% g = gower(X, Y), returns the Gower distance of observation points in Y
+%     from the sample data X.  X and Y must have the same number of columns.
+% The answer would be a column vector, with the length same as the number 
+%     of rows in X, that is, size(answer) == [size(X, 1), 1]. 
+Y_ = reshape(Y', [1, size(Y, 2), size(Y, 1)]);
+g = min(mean(abs(X - Y_) ./ range(Y, 1), 2), [], 3);
+end
+% 20190511
